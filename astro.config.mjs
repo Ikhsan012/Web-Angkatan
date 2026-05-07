b23c38b02c60438bab903d28fdd0d3ac28550a57
Ikhsan012 <ikhsannull10@gmail.com>
2026-05-07 18:09:16 +0800

    optimize update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,25 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel'; // Ganti dari '@astrojs/node'
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   image: {
     domains: ['kwcftnoczlayucmxtlrs.supabase.co']
   },
+
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
   }),
-  output: 'server', // Atau 'hybrid' tergantung kebutuhan data dinamis lu
+
+  // Atau 'hybrid' tergantung kebutuhan data dinamis lu
+  output: 'server',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
